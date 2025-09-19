959231e204b67dac0db26d52f291b3c5031f8d53
jelereduard <jelereduard@yahoo.com>
2025-09-19 21:24:40 +0300

    add jwt logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 type AuthInput = { username: string; password: string };
 type SignInData = { userId: number; username: string };
-type AuthResult = { acessToken: string; userId: number; username: string };
+type AuthResult = { accessToken: string; userId: number; username: string };
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async authenticate(input: AuthInput): Promise<AuthResult> {
     const user = await this.validateUser(input);
@@ -16,11 +20,7 @@ export class AuthService {
       throw new UnauthorizedException();
     }
 
-    return {
-      acessToken: 'fake-token',
-      userId: user.userId,
-      username: user.username,
-    };
+    return this.signIn(user);
   }
 
   async validateUser(input: AuthInput): Promise<SignInData | null> {
@@ -33,5 +33,15 @@ export class AuthService {
       };
     }
     return null;
+  }
+
+  async signIn(user: SignInData): Promise<AuthResult> {
+    const tokenPayload = {
+      sub: user.userId,
+      username: user.username,
+    };
+
+    const accessToken = await this.jwtService.signAsync(tokenPayload);
+    return { accessToken, username: user.username, userId: user.userId };
   }
 }
---
+++ src/config/jwt-secret.ts
@@ -0,0 +1,1 @@
+export const JWT_SECRET = 'b7f2e1c9-4a3d-4e5b-9c2a-8f6d3e7a1b0c'; // Random string, placeholder for jwt-secret
