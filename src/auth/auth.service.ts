a61419cc040d5715dc60a4779777681f8f283468
jelereduard <jelereduard@yahoo.com>
2025-09-19 20:57:47 +0300

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+type AuthInput = { username: string; password: string };
+type SignInData = { userId: number; username: string };
+type AuthResult = { acessToken: string; userId: number; username: string };
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async authenticate(input: AuthInput): Promise<AuthResult> {
+    const user = await this.validateUser(input);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      acessToken: 'fake-token',
+      userId: user.userId,
+      username: user.username,
+    };
+  }
+
+  async validateUser(input: AuthInput): Promise<SignInData | null> {
+    const user = await this.userService.findUserByName(input.username);
+
+    if (user && user.password === input.password) {
+      return {
+        userId: user.userId,
+        username: user.username,
+      };
+    }
+    return null;
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+};
+
+const users: User[] = [
+  {
+    userId: 1,
+    username: 'Edi',
+    password: 'secret1',
+  },
+  {
+    userId: 2,
+    username: 'Alex',
+    password: 'secret2',
+  },
+];
+
+@Injectable()
+export class UsersService {
+  async findUserByName(username: string): Promise<User | undefined> {
+    return users.find((user) => user.username === username);
+  }
+}
